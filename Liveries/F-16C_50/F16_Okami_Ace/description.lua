ce8914b6cf70c4840773df4258addaf55e7af462
Vurplex <vurplex@gmail.com>
2023-07-14 02:32:00 -0500

    Fixed 370gal fuel tanks

--- Liveries/F-16C_50/F16_Okami_Ace/description.lua
+++ Liveries/F-16C_50/F16_Okami_Ace/description.lua
@@ -157,11 +157,9 @@ livery = {
 	{"Fuel_Tank_300Gal", ROUGHNESS_METALLIC,	    "Fuel_Tank_300Gal_RoughMet",	    true};
 	
 -- F16_bl50_Fuel_Tank_370Gal
-	{"F_16_Tank_370", 0,	                        "F-16_Tank_370_diff",		        true};
-	{"F_16_Tank_370", 1,	                        "F-16_Tank_370_NM",		            true};
-	{"F_16_Tank_370", ROUGHNESS_METALLIC,	        "F-16_Tank_370_diff_RoughMet",	    true};
-	
-
+	{"F_16_Tank_370", 0,	                        "Fuel_Tank_370gal_diff",		    true};
+	{"F_16_Tank_370", 1,	                        "Fuel_Tank_370gal_NM",		        true};
+	{"F_16_Tank_370", ROUGHNESS_METALLIC,	        "Fuel_Tank_370gal_diff_RoughMet",	true};
 
 -- Pilot
 		
